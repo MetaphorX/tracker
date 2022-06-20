13824c4991ec24c97909fe59cca4b2c714234d33
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-06-20 08:41:45 +0100

    initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import {Home, Scanner, Login} from './components/index'
+
+const App = () => {
+  return (
+    <Routes>
+        <Route path="/" />
+    </Routes>
+  )
+}
+
+export default App
---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export {default as Home} from './Home'
+export {default as Product} from './Product'
+export {default as Scanner} from './Scanner'
+export {default as Login} from './Login'
