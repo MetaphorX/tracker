6d4e3f84e9be32331b61a89d0af6595c21cfabaf
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-06-23 12:33:47 +0100

    Add files via upload

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionCount;
+
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+    struct TransferStruct{
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain() public{
+        
+    }
+    function getAllTransactions() public view returns (TransferStruct[] memory){
+
+    }
+    function getTransactionCount() public view returns (uint256){
+
+    }
+}
