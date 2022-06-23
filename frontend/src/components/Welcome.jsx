17d0cda70fff4a3dba68fe9e1dc4ac9821f13902
Shakur Imperiale <56418342+MetaphorX@users.noreply.github.com>
2022-06-23 11:14:14 +0100

    fixed tailwind and added loader component

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -18,11 +18,11 @@ const Welcome = () => {
 
   const connectWallet =()=>{}
   const handleSubmit =()=>{}
-  
+
   return (
     <div className='flex w-full justify-center items-center'>
-      <div className='flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
-        <div className='flex flex-1 justify-starrt flex-col md:mr-10'>
+      <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
+        <div className='flex flex-1 justify-starrt flex-col mf:mr-10'>
           <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
              Scan and Verify <br /> All Purchased Products
           </h1>
@@ -39,7 +39,7 @@ const Welcome = () => {
           </button>
           
         </div>
-        <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+        <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
           <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
             <div className='flex justify-between flex-col w-full h-full'>
               <div className='flex justify-between items-start'>
@@ -69,8 +69,9 @@ const Welcome = () => {
               <button
               type="button"
               onClick={handleSubmit}
+              className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
               >
-
+                Generate Barcode
               </button>
             )}
           </div>
